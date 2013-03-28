701e44a94ac6e1b4deb393a16a29bcffe6af4264
fracek <francesco@ceccon.me>
2013-03-28 15:49:00 +0100

    New style

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
 
-        </section><!-- /end role["main"] -->
-        <div class="clear"></div>
-        <footer>
+            </section><!-- /end role["main"] -->
+        </div><!-- /end .page -->
+        <div id="push"></div>
+    </div><!-- /end #wrap -->
+    <footer id="footer">
+        <div class="container">
             <? if (is_active_sidebar('footer-bottom')) : ?>
                 <?php dynamic_sidebar('footer-bottom'); ?>
             <? endif; ?>
-            <?php wp_footer(); ?>
-        </footer>
-    </div><!-- /end .page -->
+        </div>
+        <?php wp_footer(); ?>
+    </footer>
 </body>
 </html>
